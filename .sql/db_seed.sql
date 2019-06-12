53db187cf76075274d834b585fc2ecff18b214dc
Salma Abdelhady <salmahabdelhady@gmail.com>
2019-06-12 12:36:00 +0200

    update seeds

--- .sql/db_seed.sql
+++ .sql/db_seed.sql
@@ -2,37 +2,43 @@ INSERT INTO socialgraph.graph (id, name)
 VALUES (1, 'testcommand');
 
 INSERT INTO socialgraph.node (id, graph_id, name, attributes)
-VALUES (1, 9, 'A', '{"nodes": 10}');
+VALUES (1, 1, 'A', '{"nodes": 10}');
 
 INSERT INTO socialgraph.node (id, graph_id, name, attributes)
-VALUES (2, 9, 'B', '{"nodes": 10}');
+VALUES (2, 1, 'B', '{"nodes": 10}');
 
 INSERT INTO socialgraph.node (id, graph_id, name, attributes)
-VALUES (3, 9, 'C', '{"nodes": 10}');
+VALUES (3, 1, 'C', '{"nodes": 10}');
 
 INSERT INTO socialgraph.node (id, graph_id, name, attributes)
-VALUES (4, 9, 'D', '{"meta": "graph#nodes"}');
+VALUES (4, 1, 'D', '{"meta": "graph#nodes"}');
 
 INSERT INTO socialgraph.node (id, graph_id, name, attributes)
-VALUES (5, 9, 'E', '{"attributes": "graph#nodes"}');
+VALUES (5, 1, 'E', '{"attributes": "graph#nodes"}');
 
 INSERT INTO socialgraph.node (id, graph_id, name, attributes)
-VALUES (6, 9, 'G', '{"attributes": "graph#nodes"}');
+VALUES (6, 1, 'G', '{"attributes": "graph#nodes"}');
 
 INSERT INTO socialgraph.edge (directed, weight, graph_id, source_id, destination_id)
-VALUES (0, 9, null, 1, 2);
+VALUES (0, 9, 1, 1, 2);
 
 INSERT INTO socialgraph.edge (directed, weight, graph_id, source_id, destination_id)
-VALUES (0, 9, null, 1, 3);
+VALUES (0, 9, 1, 1, 3);
 
 INSERT INTO socialgraph.edge (directed, weight, graph_id, source_id, destination_id)
-VALUES (0, 9, null, 2, 3);
+VALUES (0, 9, 1, 2, 3);
 
 INSERT INTO socialgraph.edge (directed, weight, graph_id, source_id, destination_id)
-VALUES (0, 9, null, 3, 4);
+VALUES (0, 9, 1, 3, 1);
 
 INSERT INTO socialgraph.edge (directed, weight, graph_id, source_id, destination_id)
-VALUES (0, 9, null, 4, 5);
+VALUES (0, 9, 1, 3, 4);
 
 INSERT INTO socialgraph.edge (directed, weight, graph_id, source_id, destination_id)
-VALUES (0, 9, null, 5, 6);
+VALUES (0, 9, 1, 5, 6);
+
+INSERT INTO socialgraph.edge (directed, weight, graph_id, source_id, destination_id)
+VALUES (0, 9, 1, 4, 6);
+
+INSERT INTO socialgraph.edge (directed, weight, graph_id, source_id, destination_id)
+VALUES (0, 9, 1, 4, 4);
